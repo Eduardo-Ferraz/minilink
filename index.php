744785a9a5865785ae8329ea7e9b52cf1b9de776
Eduardo Ferraz <eduardoc.ferraz2@gmail.com>
2022-10-08 16:50:11 -0300

    Integração ao html iniciada

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landpage</title>
+</head>
+<body>
+    <h1>
+        <?php
 
-    echo 'chama B)';
+        echo 'chama B)'
 
-    $name = 'Lucas';
+        $name = 'Lucas';
 
-    echo $name;
+        echo $name;
+        
+        ?>
+    </h1>
+</body>
+</html>
 
-?>
+
