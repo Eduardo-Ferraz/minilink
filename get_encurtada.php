2954f4fe8a38106361d5cd0419a96c04cc2ca657
Eduardo Ferraz <eduardoc.ferraz2@gmail.com>
2022-10-10 20:11:21 -0300

    tabela "url" renomeada para "links"

--- get_encurtada.php
+++ get_encurtada.php
@@ -7,7 +7,7 @@ if(isset($_POST["link"])){
     $new_url = $_POST["link"];
     //$sql = "SELECT linkIni * FROM 'url' ORDER BY id ASC;";
 
-    $sql = "INSERT INTO url(linkIni, id, fk_usuario_id) VALUES ('$new_url', '1', '2');";
+    $sql = "INSERT INTO links(linkIni, id, fk_usuario_id) VALUES ('$new_url', '1', '2');";
 			$result = $conn->query($sql);
 			$conn->commit();
 			echo "<label><span>Link enviado para ser encurtado.</span></label>";
