dbe1779019440a3bc751c8664b17a96c8eb89075
lucdnitz <lucas.nitz@hotmail.com>
2022-10-11 19:07:37 -0300

    modificado3

--- encurt_orig.php
+++ encurt_orig.php
@@ -20,17 +20,11 @@ if(isset($_POST["idUrl"])){
 
         $sql = "SELECT keyUsuario FROM usuario INNER JOIN links ON usuario.id = links.fk_usuario_id WHERE links.id='$idUrl'";
         $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
         
-        if($result->num_rows!=0 && isset($_POST['key'])){
-            $row = $result->fetch_assoc();
-            if($row['keyUsuario'] === $_POST['key']){
-                $response['msg'] = 'Url encontrada';
-                $response['link'] = $link_ini;
-                $response['success'] = 1;
-            }else{
-                $response['msg'] = 'Permissao insuficiente';
-                $response['success'] = 0;
-            }
+        if($result->num_rows!=0 && isset($_POST['key']) && $row['keyUsuario'] !== $_POST['key']){
+            $response['msg'] = 'Permissao insuficiente';
+            $response['success'] = 0;
         }else{
             $response['msg'] = 'Url encontrada';
             $response['link'] = $link_ini;
