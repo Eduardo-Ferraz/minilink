df720c7d0394acddbbb18ca66c497a0c1c6d7054
lucdnitz <lucas.nitz@hotmail.com>
2022-10-11 16:49:25 -0300

    get_encurtada finalizada

--- get_encurtada.php
+++ get_encurtada.php
@@ -1,16 +1,49 @@
 <?php
 
 include ".\connect.php";
-//$new_url = ""; Precisa disso?
+
+$response = array();
 
 if(isset($_POST["link"])){
-    $new_url = $_POST["link"];
-    //$sql = "SELECT linkIni * FROM 'url' ORDER BY id ASC;";
 
-    $sql = "INSERT INTO links(linkIni, id, fk_usuario_id) VALUES ('$new_url', '1', '2');";
-			$result = $conn->query($sql);
-			$conn->commit();
-			echo "<label><span>Link enviado para ser encurtado.</span></label>";
+    $urlSite = $_POST["link"];
+
+    $idUrl = substr(md5(microtime()), rand(0, 26), 5);
+
+    $sql = "SELECT * FROM links WHERE id='$idUrl'";
+	$result = $conn->query($sql);
+
+    while($result->num_rows!=0){
+        $idUrl = substr(md5(microtime()), rand(0, 26), 5);
+
+        $sql = "SELECT * FROM links WHERE id='$idUrl'";
+        $result = $conn->query($sql);
+    }
+
+
+    if(isset($_POST['key'])){
+
+        $sql = "SELECT id FROM usuario WHERE keyUsuario={$_POST['key']}";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $idUsuario = $row['id'];
+
+        $sql = "INSERT INTO links(link_ini, id, fk_usuario_id) VALUES ('$urlSite', '$idUrl', $idUsuario);";
+
+    }else{
+        $sql = "INSERT INTO links(link_ini, id) VALUES ('$urlSite', '$idUrl');";
+    }
+
+    $result = $conn->query($sql);
+    $conn->commit();
+    $conn->close();
+    $response['msg'] = 'Mensagem recebida com sucesso';
+    $response['success'] = 1;
+}else{
+    $response['msg'] = 'Link nao inserido';
+    $response['success'] = 0;
 }
+
+echo json_encode($response);
 
 ?>
