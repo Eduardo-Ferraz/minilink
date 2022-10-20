533409096da266381c72d7b399f1abb7ffb604bd
lucdnitz <lucas.nitz@hotmail.com>
2022-10-20 18:50:40 -0300

    error

--- mensagem.php
+++ mensagem.php
@@ -11,18 +11,23 @@ switch($errorMsg){
         break;
     case 1:
         $response['msg'] = 'Dados nao inseridos';
+        $response['success'] = 2;
         break;
     case 2:
         $response['msg'] = 'Permissao insuficiente';
+        $response['success'] = 3;
         break;
     case 3:
         $response['msg'] = 'idUrl ja existente, informe outra';
+        $response['success'] = 4;
         break;
     case 4:
         $response['msg'] = 'Url nao encontrada';
+        $response['success'] = 5;
         break;
     default:
         $response['msg'] = 'Erro desconhecido';
+        $response["success"] = 6;
         break;
 }
 
